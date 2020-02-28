181b8d843a23742fab4c0d2ebf9d540f7679bea6
Wiktor Strzębała <wiktorek140@tlen.pl>
2020-02-28 08:45:40 +0100

    msm8953-common: Enable USB configfs and add init script
    Change-Id: I0f7aabb28e0d0f3517f1132208baead0a1bdd739
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>

--- msm8953.mk
+++ msm8953.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
     init.hidl.sensor.rc \
     init.mmi.chipset.rc \
     init.mmi.overlay.rc \
-    init.mmi.usb.rc \
+    init.msm.usb.configfs.rc \
     init.mmi.rc \
     init.qcom.rc \
     init.recovery.qcom.rc \
